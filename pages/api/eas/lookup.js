3fe422829e738c33d47210aa24e72606573f5478
hshaosf <11635576+hshaosf@users.noreply.github.com>
2020-07-29 23:53:00 -0700

    linting updates and such

--- pages/api/eas/lookup.js
+++ pages/api/eas/lookup.js
@@ -1,20 +1,22 @@
-const { cors, asyncJsonHandler} = require('../../../lib/handlers')
-const axios = require('axios');
+require('dotenv').config()
+const { EAS_API_URL } = process.env
+
+const { cors, asyncJsonHandler } = require('../../../lib/handlers')
+const axios = require('axios')
 
 module.exports = cors(asyncJsonHandler((req, res) => {
   return doLookup(req.query)
 }))
 
-async function doLookup(params) {
-  EAS_API_URL = process.env.EAS_API_URL
+async function doLookup (params) {
   params.$where = `address like upper('${params.search}%') and parcel_number IS NOT NULL`
   delete params.search
-  
-  let data = await axios.get(EAS_API_URL, {params: params})
+
+  const data = await axios.get(EAS_API_URL, { params: params })
     .then(response => {
-      return response.data; 
+      return response.data
     })
-    .catch(error => console.log('Error', error));
-  
-  return {items: data}
+    .catch(error => console.log('Error', error))
+
+  return { items: data }
 }
